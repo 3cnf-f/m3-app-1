d1b827c8378245e2e4ff2c9123afad719d35d21b
farouk.hashim@gmail.com <farouk.hashim@gmail.com>
2024-05-29 09:16:14 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,22 +1,29 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-import time
+
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    counter=0
+    self.timer_1.interval=0.1
+    self.counter=0
     # Any code you write here will run before the form opens.
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    self.text_box_2.text=self.text_box_1.text
     pass
 
   def text_box_1_change(self, **event_args):
     """This method is called when the text in this text box is edited"""
-    xx=str(time.time_ns())
-    counter=counter+1
-    self.text_box_2.text=counter
+    if self.counter>self.oldcounter):
+    self.text_box_2.text=str(self.counter)
+   
+    
+    pass
+
+  def timer_1_tick(self, **event_args):
+    "This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
+    self.counter=self.counter+0.5
+    
     
     pass
